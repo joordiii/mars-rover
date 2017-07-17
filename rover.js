2f0ce5785c4ff72ecad1960253dd72f023c51fe5
Jordi Clotet <joordiii@gmail.com>
2017-07-17 12:33:48 +0200

    versio final classe2

--- rover.js
+++ rover.js
@@ -1,51 +1,42 @@
-var dir="";
-var pos="";
-var myRover = {
-  iniPos :"" ,
-  iniTopPosition: "",
-  iniLeftPosition: "" ,
-  direction: dir,
-  position: pos
-};
 
-var pixTop="";
-var pixTopPf="";
-var pixLeft="";
-var pixLeftPf="";
-//rover goes at the starting point onload
-function initialize() {
-
-for (var a=0; a<100; a=a+1) {
-  var iDiv = document.createElement('div');
-  iDiv.id = 'c' + a;
-  iDiv.className = 'rectangle';
-  var grid = document.getElementById("grid");
-  grid.appendChild(iDiv);
-
-  if (a%2 === 0){
-    var divRover = document.createElement('div');
-    divRover.id = 'rover';
-    iDiv.appendChild(divRover);
-  }
-
-
-  }
-
-
-  /*myRover = {
-  iniPos : gridPix[10][0],
-  iniTopPosition: gridPix[10][0][0],
-  iniLeftPosition: gridPix[10][0][1],
+var Rover = {
+  position: [0,0],
   direction: 'N'
-  };
-  document.getElementById("rover").style.marginTop = myRover.iniTopPosition+'px';
-  document.getElementById("rover").style.marginLeft = myRover.iniLeftPosition+'px';
-  pixTop = document.getElementById("rover").style.marginTop;
-  pixTopPf= parseFloat(pixTop);
-  document.getElementById("currentY").innerHTML=pixTopPf;
-  pixLeft = document.getElementById("rover").style.marginLeft;
-  pixLeftPf= parseFloat(pixLeft);
-  document.getElementById("currentX").innerHTML=pixLeftPf;*/
+};
+var taula = [];
+
+function putRoverNewPosition(taula,x,y){
+  var divABorrar = taula[Rover.position[0]][Rover.position[1]];
+  divABorrar.innerHTML = '';
+
+  var divAPosar = taula[x][y];
+  var divRover = document.createElement('div');
+  divRover.id = 'rover';
+  divAPosar.appendChild(divRover);
+  Rover.position[0] = x;
+  Rover.position[1] = y;
+}
+
+
+function initialize() {
+  for (var a=0; a<10; a=a+1) {
+    var fila = [];
+    for (var b=0; b<10; b=b+1) {
+      var iDiv = document.createElement('div');
+      fila.push(iDiv);
+      iDiv.id = 'c' + a + b;
+      iDiv.className = 'rectangle';
+      var grid = document.getElementById("grid");
+      grid.appendChild(iDiv);
+
+      if (a === 0 && b===0){
+        var divRover = document.createElement('div');
+        divRover.id = 'rover';
+        iDiv.appendChild(divRover);
+      }
+    }
+    taula.push(fila);
+  }
 }
 function setPosition() {
   /*document.getElementById("rover").style.marginTop = myRover.iniTopPosition+'px';
@@ -56,119 +47,13 @@ function setPosition() {
 
 //gets the position and displays the pixels in X: Y: screen
 
-function getTopPosition() {
-  pixTop = document.getElementById("rover").style.marginTop;
-  pixTopPf= parseFloat(pixTop);
-  document.getElementById("currentY").innerHTML = pixTopPf;
-  return pixTopPf;
-  }
-
-function getLeftPosition() {
-  pixLeft = document.getElementById("rover").style.marginLeft;
-  pixLeftPf= parseFloat(pixLeft);
-  document.getElementById("currentX").innerHTML = pixLeftPf;
-  return pixLeftPf;
-}
-console.log(getTopPosition());
-console.log(getLeftPosition());
-//Grid that contain pixels (padding-top and padding-left) position
-var gridPix = [];
-var count1=-85;
-var count2=-76;
-for (var i=0; i<=10; i++){
-  gridPix[i]=new Array(10);
-  count1=count1+50;
-  count2=-76;
-    for (var j=0; j<=10; j++){
-      count2=count2+50;
-      gridPix[i][j]=[count1,count2];
-    }
-}
-console.log("gridPix");
-console.log(gridPix);
-//Grid that contain coordinates position
-var gridCoor = [];
-var count3=11;
-var count4=-1;
-for (var i=0; i<=10; i++){
-  gridCoor[i]=new Array(10);
-    count3=count3-1;
-    count4=-1;
-    for (var j=0; j<=10; j++){
-      count4=count4+1;
-      gridCoor[i][j]=[count4,count3];
-    }
-}
-console.log("gridCoor");
-console.log(gridCoor);
-console.log(gridPix.length);
-var len=gridPix.length;
-//Grid that contain array position
-var gridPos = [];
-var count5=-1;
-var count6=-1;
-for (var i=0; i<=10; i++){
-  gridPos[i]=new Array(10);
-    count5=count5+1;
-    count6=-1;
-    for (var j=0; j<=10; j++){
-      count6=count6+1;
-      gridPos[i][j]=[count5,count6];
-    }
-}
-console.log("gridPos");
-console.log(gridPos);
-// Looking for an element into the array
-var item2 =[pixTopPf,pixLeftPf];
-var item = parseFloat(item2);
-console.log(item);
-function isItemInArray(gridPix,item){
-    document.getElementById("proves").innerHTML=item;
-  for (var j=0; j<gridPix.length; j++){
-    for (var k=0; k<gridPix.length; k++) {
-      if (gridPix[j][k][0]===item[0] && gridPix[j][k][1]===item[1] ) {
-        document.getElementById("currentX").innerHTML = gridPos[j][k][0];
-        document.getElementById("currentY").innerHTML = gridPos[j][k][1];
-        console.log(j);
-        console.log(k);
-        break;
-        }
-
-    }
-}
-}
-console.log(item2);
-
-
-//from pixels to coordinates
-pixToCoor();
-var a = getLeftPosition();
-var b = getTopPosition();
-function pixToCoor(gridPix,a,b) {
-  var item = [a,b];
-  console.log(item);
-  for (var j=0; j<len; j++){
-    for (var k=0; k<len; k++) {
-      if (gridPix[j][k][0]===item[0] && gridPix[j][k][1]===item[1] ) {
-        document.getElementById("currentX").innerHTML = gridPos[j][k][0];
-        document.getElementById("currentY").innerHTML = gridPos[j][k][1];
-        console.log(gridPix[j][k][0]);
-        console.log(j);
-        console.log(k);
-        document.getElementById("proves").innerHTML=item;
-        break;
-        }
-
-    }
-}
-}
 
 //function forward
 function goForward(myRover,pixLeftPf,pixTopPf) {
   switch(myRover.direction) {
     case 'N':
       /*myRover.position=gridPix[pixLeftPf, pixTopPf-50];*/
-      myRover.position=gridPix[20, 50];
+    //  myRover.position=gridPix[20, 50];
       break;
     case 'E':
       myRover.position[1]++;
